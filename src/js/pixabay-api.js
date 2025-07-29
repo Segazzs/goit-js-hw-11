d0b670e458b93b47ad2153d7a9282b6c49b8b970
Maxim_Rakul <mrakul84@gmail.com>
2025-07-29 11:18:26 +0300

    Update pixabay-api.js

--- src/js/pixabay-api.js
+++ src/js/pixabay-api.js
@@ -3,9 +3,8 @@ import axios from 'axios';
 const apiKey = '51493488-12143edce1099078d847fb8bb';
 
 export function getImagesByQuery(query) {
-  const encodedQuery = encodeURIComponent(query.trim());
+  const encodedQuery = encodeURIComponent(query.trim());
+  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodedQuery}&orientation=horizontal&image_type=photo&safesearch=true`;
 
-  const url = `https://pixabay.com/api/?key=${apiKey}&q=${encodedQuery}&orientation=horizontal&image_type=photo&safesearch=true`;
-
-  return axios.get(url);
+  return axios.get(url).then(response => response.data.hits);
 }
